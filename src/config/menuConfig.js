7280d88bc5041b3548eb254dafaba4a75ff1c6c8
Xue <1099232082@qq.com>
2019-10-21 19:50:37 +0800

    commit

--- src/config/menuConfig.js
+++ src/config/menuConfig.js
@@ -1,9 +1,8 @@
 const menuList = [
   {
-    title: "首页", // 菜单标题
-    key: '/home', // 对应的path路径
-    icon: 'home', // 图标名称
-    isPublic: true, // 公开的
+    title: '用户信息',
+    key: '/user',
+    icon: 'user'
   },
   {
     title: '数据采集模块',
@@ -32,44 +31,13 @@ const menuList = [
     key: '/assist',
     icon: 'user',
   },
-
   {
-    title: '用户管理',
-    key: '/user',
+    title: '个人中心',
+    key: '/person',
     icon: 'user'
   },
-  {
-    title: '角色管理',
-    key: '/role',
-    icon: 'safety',
-  },
 
-  {
-    title: '图形图表',
-    key: '/charts',
-    icon: 'area-chart',
-    children: [
-      {
-        title: '柱形图',
-        key: '/charts/bar',
-        icon: 'bar-chart'
-      },
-      {
-        title: '折线图',
-        key: '/charts/line',
-        icon: 'line-chart'
-      },
-      {
-        title: '饼图',
-        key: '/charts/pie',
-        icon: 'pie-chart'
-      },
-    ]
-  },
-  {
-    title: '订单管理',
-    key: '/order',
-    icon: 'windows',
-  },
+
+
 ]
 export default menuList
